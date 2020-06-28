270773bb8721b1c2f6fcc659a0e182891bae5531
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-06-28 17:15:33 -0600

    Form added in AddCategory component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Categoria 1', 'Categoria 2', 'Categoria 3']);
+
+  const handleAdd = () => {
+    setCategories([...categories, 'Harry Potter']);
+  };
+
   return (
     <>
-      <h2>GifExpertApp</h2>
-      <hr /> 
+      <h2>GifExpertApp</h2>      
+      <AddCategory setCategories={ setCategories }/>
+
+      <hr />
+
+      <ol>
+        { 
+          categories.map(categoria => <li key= {categoria}> {categoria} </li>)
+        }
+      </ol>
     </>
   );
 };
