95078d4c61ac6cd0ca5063417eac9b540212715d
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-06-28 22:51:43 -0600

    docs added

---
+++ docs/precache-manifest.e90f89996052d0e14943203012dbcd92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb3bcd7cb8dabdf428df4ab90bfdf38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a3df2cdba79257cd2e3",
+    "url": "/static/css/main.5098987c.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3df2cdba79257cd2e3",
+    "url": "/static/js/main.4d0a17ec.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
